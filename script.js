c3d369dfccc9d11ee4743c000da00bb3333c8600
simla04 <jessicasowi@gmail.com>
2022-11-14 08:29:11 +0100

    .

--- script.js
+++ script.js
@@ -10,4 +10,46 @@
 //  MILESTONE 2:
 //  Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà //  inserire i dati e visualizzare il calcolo finale con il prezzo.
 //  Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con //  massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+_______________________________________________________________________
 
+//let Kilometers = prompt('Quanti kilometri deve fare?');
+   // let EtaPersona = parseInt(prompt("Quanti anni ha?"));
+   // const priceKm = 0.21;
+   // let ticketprice = Kilometers * priceKm; 
+
+
+    //biglietto con sconto del 20%
+   // let Discount20 = ticketprice - (ticketprice * 0.2);
+
+    //biglietto con sconto del 40%
+   //let Discount40 = ticketprice - (ticketprice * 0.4);
+
+
+   // if (EtaPersona <=18){
+   // ticketprice = Discount20.toFixed(2);
+   // }
+  //  else if(EtaPersona >= 65){
+   // ticketprice = Discount40.toFixed(2);
+   // }
+   // else{
+   // ticketprice
+   // }
+   // document.getElementById('prezzo-biglietto').innerHTML = `Il prezzo //del biglietto è:  ${ticketprice} Є`;
+
+   let nameUser = document.getElementById('nameUser').value;
+   let Km = document.getElementById('Km').value;
+   let EtaPersona = document.getElementById('EtaPersona').value;
+   // let nameUser = document.getElementById('nameUser').value;
+   // let nameUser = document.getElementById('nameUser').value;
+
+
+   //
+   if (EtaPersona <=18){
+ticketprice = Discount20.toFixed(2);
+}
+   else if(EtaPersona >= 65){
+ticketprice = Discount40.toFixed(2);
+}
+else{
+ticketprice
+}
